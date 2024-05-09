24650e745e7933da1684e9bc964a61f1ad0fe622
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-05-09 17:39:21 +0200

    Add project files.

---
+++ Konstruktörerövningar5inl/Fordon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktörerövningar5inl
+{
+    class Fordon
+    {
+        private string tillvärkare;
+        private string modell;
+        private int år;
+        private int vikt;
+
+        public Fordon() 
+        {
+            tillvärkare = string.Empty;
+            modell = string.Empty;
+            år = 0;
+            vikt = 0;
+        }
+
+        public void setTillvärkare(string tillv)
+        {
+            tillvärkare = tillv;
+        }
+
+        public void setModell(string mod)
+        {
+            modell = mod;
+        }
+
+        public void setÅr(int å)
+        {
+            år = å;
+        }
+
+        public void setVikt(int vi)
+        {
+            vikt = vi;
+        }
+
+        public override string ToString()
+        {
+            return tillvärkare + " " + modell + " " + år + " " + vikt;
+        }
+    }
+}
---
+++ Konstruktörerövningar5inl/Program.cs
@@ -0,0 +1,26 @@
+﻿using Konstruktörerövningar5inl;
+using System;
+
+namespace Konstruktorerövningar5inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fordon bil1 = new Fordon();
+            bil1.setTillvärkare("Mazda");
+            bil1.setModell("Cosmo");
+            bil1.setÅr(1983);
+            bil1.setVikt(1150);
+
+            Fordon bil2 = new Fordon();
+            bil2.setTillvärkare("Toyota");
+            bil2.setModell("Chaser");
+            bil2.setÅr(1989);
+            bil2.setVikt(1280);
+
+            Console.WriteLine(bil1);
+            Console.WriteLine(bil2);
+        }
+    }
+}
